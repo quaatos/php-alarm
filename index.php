a5e546d1465daeb3648ef4c78c485f5d7e8c3e71
quaatos <93578678+quaatos@users.noreply.github.com>
2023-04-08 12:05:45 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+system("clear || cls");
 $jsonfile = file_get_contents('alarms.json');
 $decoded = json_decode($jsonfile);
 
@@ -7,12 +8,40 @@ if (isset($argv[1]) && isset($argv[2]) && isset($argv[3])) {
     $minutes = $argv[2];
     $seconds = $argv[3];
 } else {
-    if ($argv[1] == "-t") {
-        echo("Timers: " . PHP_EOL);
-        for($i = 0; $i < count($decoded); $i++) {
-            echo("\e[32m" . $i . " ==> " . $decoded[$i]->Label . " | " . $decoded[$i]->time . PHP_EOL . "\e[39m");
+    if (isset($argv)) {
+        $arguments = ['-t', '-s'];
+        for($j = 0; $j < count($argv); $j++) {
+            if (in_array($argv[$j], $arguments)) {
+                if ($argv[$j] == "-t") {
+                    echo("Timers: " . PHP_EOL);
+                    for($i = 0; $i < count($decoded); $i++) {
+                        echo("\e[32m" . $i . " ==> " . $decoded[$i]->Label . " | " . $decoded[$i]->time . PHP_EOL . "\e[39m");
+                    }
+                }
+
+                if ($argv[$j] == '-s') {
+                    $again = false;
+                    while($again = true) {
+                        system('clear || cls');
+                        echo "Setting new timer e.g: 08:30:12" . PHP_EOL;
+                        $settime = readline("New time: " . PHP_EOL);
+                        $setlabel = readline("New label: " . PHP_EOL);
+                        $sure = readline("Are you sure [Y/n/]: " . PHP_EOL);
+
+                        if (strtoupper($sure) == "Y" || $sure == "") {
+                            addTimer($settime, $setlabel);
+
+                        } elseif(strtoupper($sure) == "N") { //TODO: Fix json file writing.
+                            echo "resetting...";
+                            sleep(2);
+                            $again = true;
+                        }
+                    }
+                }
+            }
         }
     }
+    
     $option = readline("Set timer: ");
     if ($option <= count($decoded) && $option >= 0) {
         if (is_numeric($option)) {
@@ -53,5 +82,15 @@ function arrayEntry() {
     array_shift($files);
     
     return $files[array_rand($files, 1)];
+}
+
+function addTimer($time, $label) {
+    $open = fopen("alarms.json", "r+");
+    if (fwrite($open, "{\"Label\":\"$label\", \"time\":\"$time\"") . PHP_EOL) {
+        echo "Timer added! Restarting...\n";
+        fclose($open);
+        sleep(1);
+        system("php index.php -t");
+    }
 }
 ?>
